4f3ef3aa1b575ad2c013a9ce6d366cb432de314e
jonassnellinckx <jo.snel@hotmail.com>
2018-08-22 21:48:29 +0200

    add wait for load to stream tes

--- test/e2e/pages/stream.spec.js
+++ test/e2e/pages/stream.spec.js
@@ -1,12 +1,14 @@
 import chai from "chai";
 import chaiAsPromised from "chai-as-promised";
 import { harness } from "../_utils/_harness";
+import { loaded } from "../../utils";
 
 chai.should();
 chai.use(chaiAsPromised);
 
 harness("stream page", () => {
     it('should be first page', () => {
+        await loaded();
         return app.client
             .waitForExist('.auryo', 5000)
             .getText('.page-header h2')
@@ -14,8 +16,9 @@ harness("stream page", () => {
     });
 
     it('should have tracks', () => {
+        await loaded();
+
         return app.client
-            .waitForExist('.auryo', 5000)
             .getText('.page-header h2')
             .should.eventually.equal("Stream")
             .elements('.trackWrapper')
