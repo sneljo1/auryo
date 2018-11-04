7e8ba42c201145b5e20819678f21527eeb0218ac
jonassnellinckx <jo.snel@hotmail.com>
2018-11-04 17:11:50 +0100

    switch title and artist for notifications linux

--- src/main/features/NotificationManager.ts
+++ src/main/features/NotificationManager.ts
@@ -4,6 +4,7 @@ import { EVENTS } from '../../common/constants/events';
 import { getTrackEntity } from '../../common/store/entities/selectors';
 import { SC } from '../../common/utils';
 import Feature from './feature';
+import * as is from 'electron-is';
 
 export default class NotificationManager extends Feature {
 
@@ -26,12 +27,22 @@ export default class NotificationManager extends Feature {
         const trackId = playingTrack.id;
         const track = getTrackEntity(trackId)(state);
 
+
         if (track) {
           const image = SC.getImageUrl(track, IMAGE_SIZES.SMALL);
+          let title = track.title;
+          let message = `${track.user && track.user.username ? track.user.username : ''}`;
+
+          // for some reason the thing that looks like a title, is less visible than the artist, so we switch it on linux
+          if (is.linux()) {
+            const m = message;
+            message = title;
+            title = m;
+          }
 
           notifier.notify({
-            title: track.title,
-            message: `${track.user && track.user.username ? track.user.username : ''}`,
+            title,
+            message,
             icon: image,
           });
 
