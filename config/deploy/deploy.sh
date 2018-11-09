50641203d24bf75804fe5724e58d6fff2c8c0bbe
jonassnellinckx <jo.snel@hotmail.com>
2018-11-09 22:06:51 +0100

    fix deploy

--- config/deploy/deploy.sh
+++ config/deploy/deploy.sh
@@ -4,7 +4,7 @@ git config --global user.email "jonas.snellinckx@gmail.com"
 git config --global user.name "Jonas via Travis CI"
 
 current_date_time="`date +%Y%m%d%H%M%S`";
-PACKAGE_VERSION=$(cat src/package.json \
+PACKAGE_VERSION=$(cat package.json \
   | grep version \
   | head -1 \
   | awk -F: '{ print $2 }' \
@@ -16,8 +16,8 @@ cd auryo-snap
 git init
 git remote add origin https://${1}@github.com/auryo/auryo-snap.git > /dev/null 2>&1
 git pull origin master
-sed -i "s/{VERSION}/$PACKAGE_VERSION/g" ../resources/snap/snapcraft.yaml
-cp -R ../resources/snap/* ./snap
+sed -i "s/{VERSION}/$PACKAGE_VERSION/g" ../build/snap/snapcraft.yaml
+cp -R ../build/snap/* ./snap
 echo $current_date_time > triggered_build_at
 git add -A
 git commit --message "Travis build $2 for ${PACKAGE_VERSION}"
