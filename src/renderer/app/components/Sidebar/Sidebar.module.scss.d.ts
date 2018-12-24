257cb40451a8d333bedc8c1d6373c9038dd76906
philippwalter <philippwalter@pm.me>
2018-12-24 13:16:37 +0100

    fix state classes not global, move sidebar link style into module

--- src/renderer/app/components/Sidebar/Sidebar.module.scss.d.ts
+++ src/renderer/app/components/Sidebar/Sidebar.module.scss.d.ts
@@ -3,8 +3,5 @@ export const dragStrip: string;
 export const sidebarMenu: string;
 export const navLink: string;
 export const navItem: string;
-export const active: string;
-export const isCurrentPlaylist: string;
-export const isActuallyPlaying: string;
 export const breathing: string;
 export const nav: string;
