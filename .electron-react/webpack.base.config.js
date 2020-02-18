982626700c1af9c2967b7299697e434248c4232c
Jonas Snellinckx <jonas.snellinckx@gmail.com>
2020-02-18 18:21:39 +0100

    build: get proper envs on build

--- .electron-react/webpack.base.config.js
+++ .electron-react/webpack.base.config.js
@@ -2,15 +2,21 @@
 
 const webpack = require('webpack');
 const NODE_ENV = process.env.NODE_ENV || 'development';
+const Dotenv = require('dotenv-webpack');
 
 const path = require('path');
 
-const dotenv = require('dotenv').config({ path: path.join(__dirname, '..', `.env.${NODE_ENV}`) });
+const isCI =
+  process.env.CI || // Travis CI, CircleCI, Cirrus CI, Gitlab CI, Appveyor, CodeShip, dsari
+  process.env.CONTINUOUS_INTEGRATION || // Travis CI, Cirrus CI
+  process.env.BUILD_NUMBER || // Jenkins, TeamCity
+  process.env.RUN_ID;
 
 let baseConfig = {
   plugins: [
-    new webpack.EnvironmentPlugin({
-      ...dotenv.parsed
+    new Dotenv({
+      path: path.join(__dirname, '..', `.env.${NODE_ENV}`),
+      systemvars: isCI
     })
   ]
 };
