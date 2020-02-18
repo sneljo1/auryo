a358fd1db1c140da22912908eae5954b1e67e9f3
Jonas Snellinckx <jonas.snellinckx@gmail.com>
2020-02-18 17:51:04 +0100

    fix: error with dbus

--- src/main/features/linux/dbusService.ts
+++ src/main/features/linux/dbusService.ts
@@ -4,6 +4,8 @@ import { changeTrack, toggleStatus } from '@common/store/actions';
 import * as dbus from 'dbus-next';
 import { Logger, LoggerInstance } from '../../utils/logger';
 import LinuxFeature from './linuxFeature';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { app } from 'electron';
 
 export default class DbusService extends LinuxFeature {
   public readonly featureName = 'DbusService';
@@ -32,31 +34,42 @@ export default class DbusService extends LinuxFeature {
   }
 
   public async registerBindings(desktopEnv: string, session: any) {
-    const obj = await session.getProxyObject(
+    const legacy = await session.getProxyObject(
       `org.${desktopEnv}.SettingsDaemon`,
       `/org/${desktopEnv}/SettingsDaemon/MediaKeys`
     );
-
-    const player = obj.getInterface(`org.${desktopEnv}.SettingsDaemon.MediaKeys`);
-
-    player.on('MediaPlayerKeyPressed', (_: number, keyName: string) => {
-      switch (keyName) {
-        case 'Next':
-          this.store.dispatch(changeTrack(ChangeTypes.NEXT) as any);
-          break;
-        case 'Previous':
-          this.store.dispatch(changeTrack(ChangeTypes.PREV) as any);
-          break;
-        case 'Play':
-          this.store.dispatch(toggleStatus() as any);
-          break;
-        case 'Stop':
-          this.store.dispatch(toggleStatus(PlayerStatus.STOPPED) as any);
-          break;
-        default:
-      }
+    const interfaceLegacy = legacy.getInterface(`org.${desktopEnv}.SettingsDaemon.MediaKeys`);
+    interfaceLegacy.on('MediaPlayerKeyPressed', this.onMediaPlayerKeyPressed);
+    app.on('browser-window-focus', () => {
+      interfaceLegacy.GrabMediaPlayerKeys('Auryo', 0); // eslint-disable-line
     });
 
-    player.GrabMediaPlayerKeys(0, `org.${desktopEnv}.SettingsDaemon.MediaKeys`);
+    const future = await session.getProxyObject(
+      `org.${desktopEnv}.SettingsDaemon.MediaKeys`,
+      `/org/${desktopEnv}/SettingsDaemon/MediaKeys`
+    );
+    const interfaceFuture = future.getInterface(`org.${desktopEnv}.SettingsDaemon.MediaKeys`);
+    interfaceFuture.on('MediaPlayerKeyPressed', this.onMediaPlayerKeyPressed);
+    app.on('browser-window-focus', () => {
+      interfaceFuture.GrabMediaPlayerKeys('Auryo', 0); // eslint-disable-line
+    });
+  }
+
+  private onMediaPlayerKeyPressed(_: number, keyName: string) {
+    switch (keyName) {
+      case 'Next':
+        this.store.dispatch(changeTrack(ChangeTypes.NEXT) as any);
+        break;
+      case 'Previous':
+        this.store.dispatch(changeTrack(ChangeTypes.PREV) as any);
+        break;
+      case 'Play':
+        this.store.dispatch(toggleStatus() as any);
+        break;
+      case 'Stop':
+        this.store.dispatch(toggleStatus(PlayerStatus.STOPPED) as any);
+        break;
+      default:
+    }
   }
 }
