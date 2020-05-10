b969485037463f534cbeae9d98fd85a91014d976
Jonas Snellinckx <jonas.snellinckx@gmail.com>
2020-05-10 20:58:53 +0200

    fix: localize stream limit count

--- src/renderer/pages/onboarding/components/WelcomeStep.tsx
+++ src/renderer/pages/onboarding/components/WelcomeStep.tsx
@@ -5,14 +5,17 @@ interface Props {
   onNext(): void;
 }
 
+const LIMIT = 15000;
+
 export const WelcomeStep = React.memo<Props>(({ onNext }) => (
   <>
     <h1 className="animated fadeInLeft faster first">⚠️ A heads-up</h1>
     <div className="sub-title animated fadeInLeft faster second">
       Due to the limitations from the SoundCloud APIs, we are limited to{' '}
-      <strong className="text-primary">15.000 streams/day</strong>. After this, there will be a waiting period where no
-      music can be listened to. You may also notice that some music may not be available in the desktop version because
-      every song owner has the option to disable access for third party applications such as this one.
+      <strong className="text-primary">{LIMIT.toLocaleString()} streams/day</strong>. After this, there will be a
+      waiting period where no music can be listened to. You may also notice that some music may not be available in the
+      desktop version because every song owner has the option to disable access for third party applications such as
+      this one.
     </div>
 
     <div className="login_section  animated fadeInLeft faster third">
